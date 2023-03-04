1af5e334466966cf9c2fde75e4034d0d4b9bfab7
DeathCrocker <sasha_volodiin@mail.ru>
2023-03-04 15:33:18 +0300

    another version beat

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Spawn : MonoBehaviour
 {
@@ -10,15 +12,57 @@ public class Spawn : MonoBehaviour
     [SerializeField] private List<float> _bitRate;
     private int _countBit = 0;
 
+    //Song beats per minute
+    //This is determined by the song you're trying to sync up to
+    [SerializeField] private float songBpm;
+
+    //The number of seconds for each song beat
+    [SerializeField] private float secPerBeat;
+
+    //Current song position, in seconds
+    [SerializeField] private float songPosition;
+
+    //Current song position, in beats
+    [SerializeField] private float songPositionInBeats;
+
+    //How many seconds have passed since the song started
+    [SerializeField] private float dspSongTime;
+
+    //an AudioSource attached to this GameObject that will play the music.
+    [SerializeField] private AudioSource musicSource;
+    
+    [SerializeField] private float firstBeatOffset;
+    
+    
     private void Start()
     {
+        musicSource = GetComponent<AudioSource>();
+
+        //Calculate the number of seconds in each beat
+        secPerBeat = 60f / songBpm;
+
+        //Record the time when the music starts
+        dspSongTime = (float)AudioSettings.dspTime;
+
+        //Start the music
+        musicSource.Play();
+        
         StartCoroutine(BitSpawn());
-        //InvokeRepeating("SpawnEnemy", rate, rate);
+        
+    }
+
+    public void Update()
+    {
+        //determine how many seconds since the song started
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime - firstBeatOffset);
+
+        //determine how many beats since the song started
+        songPositionInBeats = songPosition / secPerBeat;
     }
 
     private IEnumerator BitSpawn()
     {
-        yield return new WaitForSeconds(_bitRate[_countBit]);
+        yield return new WaitForSeconds(secPerBeat);
         Instantiate(enemies[(int)Random.Range(0, enemies.Length)], new Vector3(Random.Range(-8.5f, 8.5f), 7, 0), Quaternion.identity); 
         
         
